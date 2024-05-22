4b4f82160666ac0dfceedd5debfb71c94d5c3ca9
masa_work <ueda@arumcode.com>
2024-05-22 10:37:07 +0900

    プロジェクト ファイルを追加します。

---
+++ WebAsmTest/Pages/Component.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WebAsmTest.Pages
+{
+	public partial class Component : ComponentBase
+	{
+		private int currentCount = 0;
+
+		private void IncrementCount()
+		{
+			currentCount++;
+		}
+
+	}
+}
---
+++ WebAsmTest/Pages/Counter.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace WebAsmTest.Pages
+{
+	public partial class Counter : ComponentBase
+	{
+		private int currentCount = 0;
+
+		private void IncrementCount()
+		{
+			currentCount++;
+		}
+
+	}
+}
